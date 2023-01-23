6d63187031886e752f28ddedb6faa0a7bf72e751
Gungde Iswara <86350332+gungde120@users.noreply.github.com>
2023-01-23 22:21:17 +0800

    Remove port config

--- app/models/db.js
+++ app/models/db.js
@@ -13,8 +13,7 @@ const connection = mysql.createConnection({
     host: process.env.HOST,
     user: process.env.USER,
     password: process.env.PASSWORD,
-    database: process.env.DB,
-    port: process.env.PORT
+    database: process.env.DB
 });
 
 connection.connect(error => {
