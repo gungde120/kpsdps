0ac906fc3179b2807697c2af2f4fadaa22f3dcad
Gungde Iswara <86350332+gungde120@users.noreply.github.com>
2023-01-23 22:18:03 +0800

    Add port config

--- app/models/db.js
+++ app/models/db.js
@@ -13,7 +13,8 @@ const connection = mysql.createConnection({
     host: process.env.HOST,
     user: process.env.USER,
     password: process.env.PASSWORD,
-    database: process.env.DB
+    database: process.env.DB,
+    port: process.env.PORT
 });
 
 connection.connect(error => {
