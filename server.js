c28718bd6fb435261c63570bdeeae06695440a90
Gungde Iswara <86350332+gungde120@users.noreply.github.com>
2023-01-24 21:03:18 +0800

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const { METHODS } = require("http");
 const app = express();
 
 var corsOptions = {
-  origin: "http://localhost:8080"
+  // origin: "http://localhost:8080"
   // methods: ["GET", "POST", "PUT", "DELETE"]
 };
 
